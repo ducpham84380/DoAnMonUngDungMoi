ac542e720d4626de945acc6f4bb153e53a778153
ducpham84380 <duc84380@gmail.com>
2023-03-14 22:20:53 +0700

    chuyen dong cho player test 1

--- public/controller/controler.js
+++ public/controller/controler.js
@@ -30,13 +30,56 @@ window.addEventListener('load',function(){
             this.width = 200;
             this.x = 0;
             this.y = this.gameHeight - this.height;
+            this.img = document.getElementById('playerImg');
+            this.frameX = 0;
+            this.frameY = 0;
+            this.speed = 0;
+            this.vy = 0;
+            this.weight = 1;
         }
         draw(context){
             context.fillStyle ='white';
             context.fillRect(this.x,this.y,this.height,this.width);
+            context.drawImage(this.img,this.frameX * this.width,this.frameY * this.height,this.width,this.height,this.x,this.y,this.width,this.height);
         }
-        update(){
-            this.x++;
+        update(input){
+            if(input.keys.indexOf('ArrowRight') > -1){
+                this.speed = 5;
+            }else{
+                if(input.keys.indexOf('ArrowLeft') > -1){
+                    this.speed = -5;
+                }else{
+                    if(input.keys.indexOf('ArrowUp') > -1 && this.onGround()){
+                        this.vy -= 32;
+                    }else{
+                        this.speed = 0;
+                    }
+                }
+            }
+            // chuyển động ngang
+            this.x += this.speed;
+            if(this.x < 0){
+                this.x = 0;
+            }else{
+                if(this.x > this.gameWidth - this.width){
+                    this.x = this.gameWidth - this.width
+                }
+            }
+            // chuyển động dọc
+            this.y += this.vy;
+            if(!this.onGround()){
+                this.vy += this.weight;
+                this.frameY = 1;
+            }else{
+                this.vy =0;
+                this.frameY = 0;
+            }
+            if(this.y > this.gameHeight - this.height){
+                this.y = this.gameHeight - this.height
+            }
+        }
+        onGround(){
+            return this.y >= this.gameHeight - this.height;
         }
     }
     
@@ -62,7 +105,7 @@ window.addEventListener('load',function(){
     function animete(){
         ctx.clearRect(0,0,canvas.width,canvas.height);
         player.draw(ctx);
-        player.update();
+        player.update(input);
         requestAnimationFrame(animete);
     }
     animete();
