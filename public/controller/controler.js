a23de4fd2cb35bb3ac9eb634a61bcfcc5d080222
ducpham84380 <duc84380@gmail.com>
2023-03-15 13:39:33 +0700

    controller 2.js

--- public/controller/controler.js
+++ public/controller/controler.js
@@ -1,7 +1,7 @@
 window.addEventListener('load',function(){
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
-    canvas.width = 800;
+    canvas.width = 1400;
     canvas.height = 720;
 
     class InputHandler{
@@ -84,15 +84,107 @@ window.addEventListener('load',function(){
     }
     
     class Backgroud{
-    
+        constructor(gameWidth, gameHeight){
+            this.gameWidth = gameWidth;
+            this.gameHeight = gameHeight;
+            this.height = 720;
+            this.width = 2400;
+            this.x = 0;
+            this.y = 0;
+            this.image = document.getElementById('backgroundImg');   
+            this.speed = 7;
+        }
+        draw(context){
+            context.drawImage(this.image,this.x,this.y,this.width,this.height);
+            context.drawImage(this.image,this.x + this.width - this.speed,this.y,this.width,this.height);
+        }
+        update(){
+            this.x -= this.speed; 
+            if(this.x < 0 - this.height){
+                this.x = 0;
+            }
+        }
     }
     
-    class Enemy{
-    
+    class Enemy {
+        constructor(gameWidth, gameHeight) {
+            this.gameWidth = gameWidth;
+            this.gameHeight = gameHeight;
+            this.width = 160;
+            this.height = 119;
+            this.image = document.getElementById("enemyImg");
+            this.x = this.gameWidth;
+            this.y = this.gameHeight - this.height;
+            this.frameX = 0;
+            this.maxFrame = 5;
+            this.fps = 20;
+            this.frameTimer = 0;
+            this.frameInterval = 1000 / this.fps;
+            this.speed = 5;
+            this.markForDeletion = false;
+        }
+        draw(context) {
+            // context.strokeStyle = "white";
+            // context.strokeRect(this.x, this.y, this.width, this.height);
+            // context.beginPath();
+            // context.arc(
+            //   this.x + this.width / 2,
+            //   this.y + this.height / 2,
+            //   this.width / 2,
+            //   0,
+            //   2 * Math.PI
+            // );
+            // context.stroke();
+            // context.strokeStyle = "blue";
+            // context.beginPath();
+            // context.arc(this.x, this.y, this.width / 2, 0, 2 * Math.PI);
+            // context.stroke();
+            context.drawImage(
+                this.image,
+                this.frameX * this.width,
+                0,
+                this.width,
+                this.height,
+                this.x,
+                this.y,
+                this.width,
+                this.height
+            );
+        }
+        update(deltaTime) {
+            if (this.frameTimer > this.frameInterval) {
+                if (this.frameX >= this.maxFrame) {
+                this.frameX = 0;
+                } else {
+                this.frameX++;
+                }
+                this.frameTimer = 0;
+            } else {
+                this.frameTimer += deltaTime;
+            }
+        
+            this.x -= this.speed;
+            if (this.x < 0 - this.width) {
+                this.markForDeletion = true;
+                score++;
+            }
+        }
     }
     
-    function handleEnemies(){
-    
+    function handleEnemies(deltaTime){
+        if (enemyTimer > enemyInterval + randomEmemyInterval) {
+            enemies.push(new Enemy(canvas.width, canvas.height));
+            console.log(enemies);
+            randomEmemyInterval = Math.random() * 1000 + 500;
+            enemyTimer = 0;
+        }else{
+            enemyTimer += deltaTime;
+        }
+        enemies.forEach((enemy) => {
+            enemy.draw(ctx);
+            enemy.update(deltaTime);
+        });
+        enemies = enemies.filter((enemy) => !enemy.markForDeletion);
     }
     
     function displayStausText(){
@@ -101,9 +193,12 @@ window.addEventListener('load',function(){
     
     const input = new InputHandler();
     const player = new Player(canvas.width, canvas.height);
+    const background = new Backgroud(canvas.width, canvas.height);
 
     function animete(){
         ctx.clearRect(0,0,canvas.width,canvas.height);
+        background.draw(ctx);
+        background.update();
         player.draw(ctx);
         player.update(input);
         requestAnimationFrame(animete);
