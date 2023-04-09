6e7a06a2b4ca6d3ef84c88b2a930eb027122c204
luuthoaihung <hunggokai@gmail.com>
2023-04-09 15:13:42 +0700

    NGDNGMGT-20 <man hinh xuat diem nguoi choi>

---
+++ public/controller/viewPoint.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+    var url = "http://localhost:3000";
+
+    var qs = (function(a) {
+        if (a == "") return {};
+        var b = {};
+        for (var i = 0; i < a.length; ++i)
+        {
+            var p=a[i].split('=', 2);
+            if (p.length == 1)
+                b[p[0]] = "";
+            else
+                b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+        }
+        return b;
+    })(window.location.search.substr(1).split('&'));
+
+    var point = document.getElementById("point");
+    var name = document.getElementById("name");
+    point.innerHTML += qs["score"];
+    name.innerHTML += qs["name"];
+
+
+    $("#btnHome").click(function(){
+        if(qs["name"].length==0 || qs["score"].length==0 ){
+            alert("lỗi");
+        }else{
+            $.post(url + "/game/update", {
+                    NamePlayer:qs["name"],
+                    Point:qs["score"],
+                    Id:qs["Id"]
+            }, function(data){
+                if(data.kq == 1){
+                    alert(data.errMsg);
+                }else{
+                    alert(data.errMsg);
+                }
+            });
+        }
+    });
+})
